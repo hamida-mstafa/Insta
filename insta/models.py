859ae3ce5f9567c07ce1c4418fbe7bfd319996eb
hamida-mstafa <hamidamstafa@gmail.com>
2018-12-16 18:53:43 +0300

    called image class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE,related_name="yg")
+    title = models.CharField(max_length=60, null=True)
+    time_created = models.DateTimeField(auto_now=True, auto_now_add=False)
+    likes = models.IntegerField(default=0, null=True)
+    caption = models.CharField(max_length=140, default="")
+    user_profile = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('home')
+    def save_image(self):
+        self.save()
